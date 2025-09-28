352cbbf7cad1e875c8e5cda3a22c6be8e9967d82
KulsumaS <19-kulsuma.sobur@whsg.info>
2025-09-28 21:12:34 +0100

    28/09/25 21:21
    before I delete the player animator

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -2,17 +2,36 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+
+    public Animator anim;
+    
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float movespeed;// allows us to controll the speed of the player
+   
+    private float x;
+    private float y;
+    public float movespeed;// allows us to controll the speed of the player
     public Vector3 playerMoveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {//get x an y values, it will always be either +1,-1 or 0
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private Vector2 input;
+    private bool moving;
+// Update is called once per frame
+    private void Update()
+    {
+        GetInput();
+        Animate();
+    }
 
-        playerMoveDirection = new Vector2(inputX,inputY).normalized;
+    
+    void GetInput()
+    {//get x an y values, it will always be either +1,-1 or 0
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        playerMoveDirection = new Vector2(x,y).normalized;
          // wants it in a vector format, sets horizontal and vertical forces
     }
 
@@ -21,5 +40,25 @@ public class PlayerContoller : MonoBehaviour
         rb.linearVelocity = new Vector2(playerMoveDirection.x * movespeed,
                 playerMoveDirection.y * movespeed);
     }
+
+    private void Animate()
+    {
+        if(input.magnitude > 0.1f || input.magnitude < -0.1f) //if the the input is grster than one or less than then movemnt is true
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+        if (moving)
+        {
+            anim.SetFloat("X", x);
+            anim.SetFloat("Y", y);
+            
+        }
+        anim.SetBool("Moving", moving); 
+    }
+    
 }
   
