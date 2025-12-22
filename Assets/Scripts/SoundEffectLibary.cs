ee332972ecbc4f752a7dcbf4397300d42cff61bc
KulsumaS <19-kulsuma.sobur@whsg.info>
2025-12-22 22:50:10 +0000

    22/12/25-22:49
    the sound effect script is working?

--- Assets/Scripts/SoundEffectLibary.cs
+++ Assets/Scripts/SoundEffectLibary.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class SoundEffectLibary : MonoBehaviour
 {
-    [UnityEngine.SerializeFeild] private SoundEffectGroup[] soundEffectGroups;
-    private Dictionary<string, List<AudioClip>> soundDictionary; // creates a dictionary for the soundeffect 
+    [SerializeField] private SoundEffectGroup[] soundEffectGroups;
+    private Dictionary<string, List<AudioClip>> soundDictionary;
 
     private void Awake()
     {
@@ -16,27 +13,31 @@ public class SoundEffectLibary : MonoBehaviour
 
     private void InitializeDictionary()
     {
-        soundDictionary = new Dictionary<string, List<AudioCip>>();
-        foreach (SoundEffectGroup soundEffectGroup in soundEffectGroups)
-        {
-            soundDictionary[soundEffectGroup.name] = soundEffectGroup.audioClips;
-        }
+       soundDictionary = new Dictionary<string, List<AudioClip>>();
+       foreach (SoundEffectGroup soundEffectGroup in soundEffectGroups)
+       {
+           soundDictionary[soundEffectGroup.name] = soundEffectGroup.audioClips;//name functions as a key, so that the audio clips cna be stored in the dictionary  under that key  
+       }
     }
 
     public AudioClip GetRandomClip(string name)
     {
-        if (soundDictionary.ContainsKey(name))//checks if the key is in the list 
+        //check if it exits in the libary 
+        if (soundDictionary.ContainsKey(name))
         {
             List<AudioClip> audioClips = soundDictionary[name];
             if (audioClips.Count > 0)
+            {
                 return audioClips[Random.Range(0, audioClips.Count)];
+            }
         }
         return null;
     }
+
 }
-[System.Serializable]
-public struct SoundEffectGroup// allows us to access the dictiionary from within unitys inspector
+[System.Serializable] //allows us to view dictionary from within the unity inspector
+public struct SoundEffectGroup
 {
-    public string name;//key for the dictionary 
+    public string name;
     public List<AudioClip> audioClips;
-}   
+}
